59589fc56c829de96c69e4c659318ae6ad59046a
Dzmitry Shchamialiou <DimQ1@ya.ru>
2021-11-22 12:10:55 +0300

    update file size (#3)

--- VoskApi/Program.cs
+++ VoskApi/Program.cs
@@ -20,7 +20,11 @@ namespace VoskApi
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>()
+                        .UseKestrel(options =>
+                        {
+                            options.Limits.MaxRequestBodySize = 209715200;
+                        });
                 });
     }
 }
